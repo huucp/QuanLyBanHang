d479e7e63bf761bed8d83ff01c5f1685a3177ba2
huucp <phamchinhhuu@gmail.com>
2012-12-19 23:43:11 +0700

    Initialize

---
+++ QuanLyBanHang.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace QuanLyBanHang
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(145, 33);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(191, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(48, 40);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(49, 13);
+            this.searchLabel.TabIndex = 1;
+            this.searchLabel.Text = "Tìm kiếm";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(368, 30);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "Tìm";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 285);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.searchBox);
+            this.Name = "mainForm";
+            this.Text = "Quan Ly Gia Hang";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button searchBtn;
+    }
+}
+
